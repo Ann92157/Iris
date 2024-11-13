0c51efa68f81a9a877ec34e9cb25163d9447166b
Ann92157 <anna.kosach57@mail.ru>
2024-11-13 06:10:54 +0300

    Add files via upload

---
+++ graphics.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def visualize_data():
+    snsdata = pd.read_csv(r"C:\Users\Анна\Desktop\НГУ\1 семестр\Iris.csv").drop(['Id'], axis=1)
+    g = sns.pairplot(snsdata, hue='Species', markers='x')
+    g = g.map_upper(plt.scatter)
+    g = g.map_lower(sns.kdeplot)
+    plt.savefig(f"data.png")
+    plt.show()
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 from data import load_data, split_data
+from graphics import visualize_data
 from models import create_linear_model, create_logistic_model
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 import yaml
@@ -11,7 +12,11 @@ def load_config(config_path):
 
 #Обучение моделей
 def train_models(config):
-    X, y = load_data("Iris.csv")
+
+    #Визуализация исходных данных
+    visualize_data()
+
+    X, y = load_data(r"C:\Users\Анна\Desktop\НГУ\1 семестр\Iris.csv")
     X_train, X_test, y_train, y_test = split_data(
         X, y, config["test_size"], config["random_state"]
     )
@@ -24,8 +29,8 @@ def train_models(config):
     accuracy_lin = accuracy_score(y_test, y_pred_class_lin)
     precision_lin = precision_score(y_test, y_pred_class_lin)
     recall_lin = recall_score(y_test, y_pred_class_lin)
-
-    #Вывод метрик модели
+    
+    #Вывод метрик
     print(f"Линейная регрессия - Accuracy: {accuracy_lin:.2f}")
     print(f"Линейная регрессия - Precision: {precision_lin:.2f}")
     print(f"Линейная регрессия - Recall: {recall_lin:.2f}")
@@ -34,17 +39,18 @@ def train_models(config):
     logistic_model = create_logistic_model(
         config["random_state"], config["C"]
     )
+
     logistic_model.fit(X_train, y_train)
     lr_y_pred = logistic_model.predict(X_test)
     accuracy_log = accuracy_score(y_test, lr_y_pred)
     precision_log = precision_score(y_test, lr_y_pred)
     recall_log = recall_score(y_test, lr_y_pred)
-    
-    #Вывод метрик модели
+
+    #Вывод метрик
     print(f"Логистическая регрессия - Accuracy: {accuracy_log:.2f}")
     print(f"Логистическая регрессия - Precision: {precision_log:.2f}")
     print(f"Логистическая регрессия - Recall: {recall_log:.2f}")
 
 if __name__ == "__main__":
-    config = load_config("config.yaml")
+    config = load_config(r"C:\Users\Анна\Desktop\НГУ\1 семестр\1_\config.yaml")
     train_models(config)
