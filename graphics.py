df875838a41afc87f020485e217081eb68518856
Ann92157 <anna.kosach57@mail.ru>
2024-11-13 06:13:27 +0300

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def visualize_data():
-    snsdata = pd.read_csv(r"Iris.csv").drop(['Id'], axis=1)
+    snsdata = pd.read_csv("Iris.csv").drop(['Id'], axis=1)
     g = sns.pairplot(snsdata, hue='Species', markers='x')
     g = g.map_upper(plt.scatter)
     g = g.map_lower(sns.kdeplot)
