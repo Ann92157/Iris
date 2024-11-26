a515553e5bf764be019a324e06725dfe7e1baf1a
Ann92157 <anna.kosach57@mail.ru>
2024-11-26 22:17:45 +0300

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -10,9 +10,9 @@ def visualize_data():
     plt.savefig(f"data.png")
     plt.show()
 
-def plot_linear(X, y, lin_reg):
+def plot_linear(X, y, linear_model):
     x_values = np.linspace(X['PetalLengthCm'].min(), X['PetalLengthCm'].max())
-    y_values = -(-0.5 + lin_reg.intercept_ + lin_reg.coef_[0] * x_values) / lin_reg.coef_[1]
+    y_values = -(-0.5 + linear_model.intercept_ + linear_model.coef_[0] * x_values) / linear_model.coef_[1]
     plt.scatter(X['PetalLengthCm'], X['PetalWidthCm'], c=y)
     plt.plot(x_values, y_values, color='green')
     plt.xlabel('PetalLengthCm')
