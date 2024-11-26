7d1c78cbfa797a78b665bfefda9976c4a0ad14ea
Ann92157 <anna.kosach57@mail.ru>
2024-11-26 22:20:06 +0300

    Update train.py

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from graphics import visualize_data
 from graphics import plot_linear
 from models import create_linear_model, create_logistic_model
 import pandas as pd
+import numpy as np
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 import yaml
 import kagglehub
