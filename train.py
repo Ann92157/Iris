e61d8434653204dcc453c30741dd4e75208ff298
Ann92157 <anna.kosach57@mail.ru>
2024-11-26 22:21:30 +0300

    Update train.py

--- train.py
+++ train.py
@@ -6,8 +6,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 import yaml
-import kagglehub
-import os
+
 
 #Загрузка конфига
 def load_config(config_path):
